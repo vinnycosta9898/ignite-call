4a4b36a75284e5b582ede56fde61cc826bdb9d06
Vinicius Costa <viniciuscosta9898@icloud.com>
2024-01-17 09:09:14 -0300

    Criando a pagina de Schedule

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,57 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { Container, UserHeader } from './styles'
+import { GetStaticProps } from 'next'
+import { prismaClient } from '@/lib/prisma'
+
+interface ScheduleProps {
+    user:{
+        name: string
+        bio: string
+        avatar_url: string
+    }
+}
+
+// Quando a pagina é estatica mais a rota é dinamica
+
+export async function getStaticPaths() {
+    return {
+        paths: [],
+        fallback: 'block'
+    }
+}
+
+export default function Scheduyle({ user } : ScheduleProps) {
+    return (
+        <Container>
+            <UserHeader>
+                <Avatar src={user.avatar_url} />
+                <Heading>{user.name}</Heading>
+                <Text>{user.bio}</Text>
+            </UserHeader>
+        </Container>
+    )
+}
+
+export const getStaticProps : GetStaticProps = async ({ params }) => {
+    const username = String(params?.username)
+
+    const user = await prismaClient.user.findUnique({
+        where:{
+            username
+        }
+    })
+
+    if(!user){
+        return{
+            notFound: true
+        }
+    }
+    
+    return{
+        props:{
+            name: user.name,
+            bio: user.bio,
+            avatarUrl: user.avatar_url
+        }
+    }
+}
