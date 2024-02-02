00015b1601d3520ccda1daa84fc274565fbf1ec6
Vinicius Costa <viniciuscosta9898@icloud.com>
2024-02-02 17:26:52 -0300

    Criando a rota de schedulling

---
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -0,0 +1,69 @@
+import dayjs from 'dayjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+import { prismaClient } from '../../../../lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+
+  const user = await prismaClient.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(400).json({ message: 'User does not exist.' })
+  }
+
+  const createSchedulingBody = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    observation: z.string(),
+    date: z.string().datetime(),
+  })
+
+  const { name, email, observation, date } = createSchedulingBody.parse(
+    req.body,
+  )
+
+  const schedulingDate = dayjs(date).startOf('hour')
+
+  if (schedulingDate.isBefore(new Date())) {
+    return res.status(400).json({
+      message: 'Date is in the past.',
+    })
+  }
+
+  const conflictingScheduling = await prismaClient.scheduling.findFirst({
+    where: {
+      user_id: user.id,
+      date: schedulingDate.toDate(),
+    },
+  })
+
+  if (conflictingScheduling) {
+    return res.status(400).json({
+      message: 'There is another scheduling at at the same time.',
+    })
+  }
+
+  await prismaClient.scheduling.create({
+    data: {
+      name,
+      email,
+      observation,
+      date: schedulingDate.toDate(),
+      user_id: user.id,
+    },
+  })
+
+  return res.status(201).end()
+}
